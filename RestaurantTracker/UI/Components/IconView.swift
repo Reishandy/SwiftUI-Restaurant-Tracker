8fb41e783aabefb6e9075463ab2b448491eddec3
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-03-11 16:30:10 +0800

    First screen about done

---
+++ RestaurantTracker/UI/Components/IconView.swift
@@ -0,0 +1,18 @@
+//
+//  IconView.swift
+//  RestaurantTracker
+//
+//  Created by Muhammad Akbar Reishandy on 11/03/26.
+//
+
+import SwiftUI
+
+struct IconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconView()
+}
