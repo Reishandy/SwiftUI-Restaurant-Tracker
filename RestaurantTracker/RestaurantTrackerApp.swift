1c94317e1252823fc5f98695319de3823ccc7e70
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-03-11 08:58:14 +0800

    Initial Commit

---
+++ RestaurantTracker/RestaurantTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantTrackerApp.swift
+//  RestaurantTracker
+//
+//  Created by Muhammad Akbar Reishandy on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
